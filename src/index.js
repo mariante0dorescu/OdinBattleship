bbe03650b65ceca7e03d2cdbd9571ab04429e425
mariante0dorescu <broinkus@gmail.com>
2023-09-11 14:33:13 +0300

    basic setup for ui

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import "./styles.css";
 import "./styles.scss";
+import UI from './ui.js'
 console.log("hello world!");
+
+// initial content load event
+document.addEventListener('DOMContentLoaded', UI.createHeader())
+document.addEventListener('DOMContentLoaded', UI.createUserNameForm())
+document.addEventListener('DOMContentLoaded', UI.createFooter())
