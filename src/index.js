e86c06ba65549f878d4a46ddabfb8109e6db7da7
mariante0dorescu <broinkus@gmail.com>
2023-09-12 12:06:02 +0300

    basic setup finished

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ console.log("hello world!");
 
 // initial content load event
 document.addEventListener('DOMContentLoaded', UI.createHeader())
+document.addEventListener('DOMContentLoaded', UI.createMessageBox())
 document.addEventListener('DOMContentLoaded', UI.createUserNameForm())
 document.addEventListener('DOMContentLoaded', UI.createGameBoardsContainer())
+document.addEventListener('DOMContentLoaded', UI.createShipsContainer())
+document.addEventListener('DOMContentLoaded', UI.createButtonsContainer())
 document.addEventListener('DOMContentLoaded', UI.createFooter())
--- src/ui.js
+++ src/ui.js
@@ -1,19 +1,44 @@
+import Ship from './Ship'
+
 export default class UI {
   static createHeader(){
-    console.log('creating header')
+    const container = document.querySelector('.container');
+    const headerDiv = document.createElement('div');
+    headerDiv.classList.add('header')
+    headerDiv.innerHTML = '<h1>BATTLESHIPS</h1>'
+    container.append(headerDiv);
   }
 
   static createUserNameForm(){
-    console.log('creating user name form')
+    const container = document.querySelector('.container');
+    const formDiv = document.createElement('div');
+    formDiv.classList.add('username-form');
+    formDiv.innerHTML = `
+    <form novalidate action="#" >
+      <h2>Enter your player name:</h2>
+        <ul>
+          <li>
+            <input type="text" name="uname" id="uname">
+            <span class="error" aria-live="polite"></span>
+          </li>
+          <li>
+            <button>Play</button>
+          </li>
+        </ul>
+    </form>
+      `
+      container.append(formDiv);
   }
 
   static createMessageBox(){
-    console.log('creating message box')
+    const container = document.querySelector('.container');
+    const messageDiv = document.createElement('div');
+    messageDiv.classList.add('messages')
+    container.append(messageDiv);
   }
 
   static createGameBoardsContainer(){
     const container = document.querySelector('.container');
-    console.log(container)
     const gameboards = document.createElement('div');
     gameboards.classList.add('gameboards');
     gameboards.appendChild(this.createGameboard('user'))
@@ -34,6 +59,41 @@ export default class UI {
     }
 
     return gameboard;
+  }
+
+  static createShipsContainer(){
+    const container = document.querySelector('.container');
+
+    const shipsContainer = document.createElement('div');
+    shipsContainer.setAttribute('class', 'ships')
+
+    const ships = [{name: 'destroyer', length: 2}, {name: 'submarine', length: 3 }, {name: 'cruiser', length: 4}, {name: 'battleship', length: 5}];
+
+    for (const ship of ships) {
+      const shipDiv = document.createElement('div');
+      shipDiv.classList.add(ship.name);
+      shipDiv.style.width = `${ship.length * 20}px`;
+      shipDiv.setAttribute('draggable', 'true')
+      shipsContainer.append(shipDiv);
+    }
+    
+    container.appendChild(shipsContainer);
+  }
+
+  static createButtonsContainer(){
+    const container = document.querySelector('.container');
+    const buttonsContainer = document.createElement('div');
+    buttonsContainer.setAttribute('class', 'buttons')
+    buttonsContainer.append(this.createButton('flip-button', 'FLIP'))
+    buttonsContainer.append(this.createButton('start-button', 'START'))
+    container.append(buttonsContainer)
+  }
+
+  static createButton(id, text) {
+    const button = document.createElement('button');
+    button.id = id;
+    button.innerText = text;
+    return button;
   }
 
   static createFooter() {
